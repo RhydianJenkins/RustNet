1fd420b19ab2c3406d0fd29962d434ed97b65b92
Rhydian Jenkins <rhydz@msn.com>
2023-05-03 20:13:04 +0100

    Add eslint checking

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,13 +1,13 @@
-import type { Component } from 'solid-js';
+import type { Component } from "solid-js";
 
-import logo from './logo.svg';
-import styles from './App.module.css';
+import logo from "./logo.svg";
+import styles from "./App.module.css";
 
 const App: Component = () => {
   return (
     <div class={styles.App}>
       <header class={styles.header}>
-        <img src={logo} class={styles.logo} alt="logo" elementtiming={''} fetchpriority={'high'} />
+        <img src={logo} class={styles.logo} alt="logo" elementtiming={""} fetchpriority={"high"} />
         <p>
           Edit <code>src/App.tsx</code> and save to reload.
         </p>

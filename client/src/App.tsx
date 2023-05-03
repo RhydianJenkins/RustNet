c85db46101aae65651bd08a921a5c242f6e4a7de
Rhydian Jenkins <rhydz@msn.com>
2023-05-03 20:34:31 +0100

    More removal

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,11 +1,10 @@
 import type { Component } from "solid-js";
 
-// eslint-disable-next-line no-unused-vars
 import styles from "./App.module.css";
 
 const App: Component = () => {
   return (
-    <h1>This is an empty app!</h1>
+    <h1 class={styles.header}>This is an empty app!</h1>
   );
 };
 

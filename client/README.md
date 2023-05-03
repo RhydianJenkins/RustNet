963390df7051a1c67cdce4e3e5bcfc13032325b1
Rhydian Jenkins <RhydianJenkins@users.noreply.github.com>
2023-05-03 21:04:17 +0100

    Add client with init solid app (#2)

---
+++ client/README.md
@@ -0,0 +1,17 @@
+# Solid Client for RustNet
+
+## Development
+
+```bash
+# from this directory
+$ npm i
+$ npm run dev
+```
+
+## Deployment
+
+```bash
+npm run build
+```
+
+You can deploy the `dist` folder to any static host provider (netlify, surge, now, etc.)
---
+++ client/src/App.tsx
@@ -0,0 +1,14 @@
+import type { Component } from "solid-js";
+
+import styles from "./App.module.css";
+
+const App: Component = () => {
+  return (
+    <main class={styles.container}>
+      <h1>Rust Net</h1>
+      <p>TODO canvas and fetch and stuff...</p>
+    </main>
+  );
+};
+
+export default App;

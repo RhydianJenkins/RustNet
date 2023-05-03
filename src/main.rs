976b5e8ee2a2c98a389c47e749cc356c71df4394
Rhydian Jenkins <rhydz@msn.com>
2023-05-03 19:12:43 +0100

    Serialize a ResponseBody instead of a string response

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,11 @@
-use actix_web::{get, post, App, HttpResponse, HttpServer, Responder};
+use actix_web::{get, post, web, App, HttpResponse, HttpServer, Responder};
 use neural_network::generate_predictions;
+use serde::Serialize;
+
+#[derive(Serialize)]
+struct ResponseBody {
+    testing: f64,
+}
 
 #[get("/checkhealth")]
 async fn checkhealth() -> impl Responder {
@@ -10,7 +16,9 @@ async fn checkhealth() -> impl Responder {
 async fn test() -> impl Responder {
     let predictions = generate_predictions().unwrap_or(0.0);
 
-    HttpResponse::Ok().body(predictions.to_string())
+    web::Json(ResponseBody {
+        testing: predictions,
+    })
 }
 
 #[actix_web::main]

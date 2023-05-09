556f10c6d3429ec5b05d7708b66c493b00b7995b
Rhydian Jenkins <rhydz@msn.com>
2023-05-09 19:51:14 +0100

    Update inputs and desired outputs

--- server/src/neural_network/mod.rs
+++ server/src/neural_network/mod.rs
@@ -4,16 +4,20 @@ mod random_float_generator;
 
 use network::Network;
 
-use self::random_float_generator::gen_random_floats;
-
 pub fn generate_predictions() -> Result<Vec<f64>, ()> {
     let network = &mut Network::new();
 
-    let inputs = gen_random_floats(16);
-    let desired_outputs = vec![0.1, 0.2, 0.3, 0.4, 0.5, 0.6, 0.7, 0.8, 0.9];
-    network.train(&inputs, &desired_outputs);
+    // TODO: Make this come from the grid values of the user's canvas
+    let pixel_values = vec![
+        0.1, 0.2, 0.3, 0.4, 0.5, 0.6, 0.7, 0.8, 0.9, 0.1, 0.2, 0.3, 0.4, 0.5, 0.6, 0.7,
+    ];
 
-    let (_, _, predictions) = network.feed_forward(&inputs);
+    // pretend the user drew a 9
+    let desired_outputs = vec![0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 1.0];
+
+    network.train(&pixel_values, &desired_outputs);
+
+    let (_, _, predictions) = network.feed_forward(&pixel_values);
 
     Ok(predictions)
 }
